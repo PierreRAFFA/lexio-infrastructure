1cf71a4f7c9b3fb477e09587c7fb9e8da8ed9ae3
Pierre Raffa <pierre.raffa@medshr.net>
2017-05-12 21:36:37 +0100

    Cleanup

--- wordz-api-mongo/setup.js
+++ wordz-api-mongo/setup.js
@@ -1,20 +1,10 @@
-let error = true;
-
 let res = [
   db.getName(),
-  db.auth("admin", "password"),
-  purchase = db.getSiblingDB('wordz-purchase'),
-  purchase.createUser({ user: "api", pwd: "password", roles: ["readWrite"] })
-]
+  db.auth('admin', 'password'),
+  mainDb = db.getSiblingDB('wordz-api'),
+  mainDb.createUser({ user: 'api', pwd: 'password', roles: ['readWrite'] })
+];;
 
-print("==============")
-print(db)
-printjson(res)
-print("==============")
-// printjson(res);
-
-// if (error) {
-//   print('Error, exiting')
-//   print(error)
-//   quit(1)
-// }
+print('==============');
+printjson(res);
+print('==============');
